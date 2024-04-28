3490928adf14d6a864b9667ce799365fb69385d8
raiesbo <raiesbo@gmail.com>
2024-04-28 11:28:05 +0200

    implement home page draft using templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -13,7 +15,43 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// the second parameter is the header value.
 	w.Header().Add("Server", "Go")
 
-	w.Write([]byte("hello from Snippetbox"))
+	// Initialize a slice contaiing the paths to the two files. It's important
+	// to note taht the file containing our base template must be the FIRST
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl", // Include the navigation partial in the template files.
+	}
+
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message, use
+	// The http.Error() fucntio to send an  Internal Server Error response to the
+	// user, and then return from the handler so no subsequent code is executed.
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a tesmplate set. Notice that we use ... to pass the contents
+	// of the files slice as variadic arguments.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Then we use the Execute() method on the template set to write the
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	// leave as nil.
+
+	// Uset the ExecuteTemplate() method to write the content of the "base"
+	// template as the reponse body.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	// w.Write([]byte("hello from Snippetbox"))
 }
 
 // Add a snippetView handler function.
