37b14dd63db73fc767587c893f5235b52e409c56
raiesbo <raiesbo@gmail.com>
2024-04-28 22:05:49 +0200

    implement static files handler and add resources

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,17 @@ func main() {
 	// use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
+
+	// Create a file server shich serves files out of the "./ui/static" directory.
+	// Notes that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register the other routes as normal...
 	// The "{$}" prevents trailing slash URLs from becoming "catch it all"
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snipppetView) // Add the {id} wildcard segment
