2406592b22ff14f209709381406929555429b994
raiesbo <raiesbo@gmail.com>
2024-09-17 22:23:56 +0200

    implement Authenticate user method

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/lib/pq"
@@ -52,7 +53,34 @@ VALUES ($1, $2, $3, NOW()) RETURNING id;`
 // the provided email address and password. This will return the relevant
 // user ID if they do.
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	// Retrieve the id and hashed password associated with the given email. If
+	// no matching emaial exists we return the ErrInvalidCredentials error.
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = $1;"
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCrredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	// Check whether the hashed pasword and plain-test password provided match.
+	// If they don't, we reutnr the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCrredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	// Otherwise, the password is correct. Return the user ID
+	return id, nil
 }
 
 // We'll use the Exists method to check if a user exists with a specific ID.
