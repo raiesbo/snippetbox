cdde3ea3e66676111f41315397f30aad9ec7cf26
raiesbo <raiesbo@gmail.com>
2024-10-13 20:47:16 +0200

    extend humanDate tests and improve function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -27,6 +27,12 @@ type templateData struct {
 // Create a humanDate function which returns a nicely formatted string
 // representation of a time.Time object,
 func humanDate(t time.Time) string {
+	// Return the empty string if time ahs the zero value.
+	if t.IsZero() {
+		return ""
+	}
+
+	// Convert the time to UTC before formatting it.
 	return t.Format("02 Jan 2006 at 15:04")
 }
 
--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,15 +6,54 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	// Initialize a new time.Time object and pass it to the humanDate function.
-	tm := time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC)
-	hd := humanDate(tm)
-
-	// Check that the output from the humanDate function is in the format we
-	// expect. If it isn't what we expect, use the t.Errorf() funciton to
-	// indicate that the test has failed and log the expected and actual values.
-	expectedResult := "17 Mar 2024 at 10:15"
-	if hd != expectedResult {
-		t.Errorf("got %q; want %q", hd, expectedResult)
+	// Create a slice of anonymous structs containing the test case name,
+	// input to out humanDate() funciton (the tmb field), and expected output
+	// (the want field).
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC),
+			want: "17 Mar 2024 at 10:15",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2024, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar 2024 at 10:15",
+		},
 	}
+
+	// Loop over the test cases.
+	for _, tt := range tests {
+		// use the t.Run() function to run a sub-test for each test case. The
+		// first parameter to this is the name of the test (which is used to
+		// identify the sub-test in any log output) and the second paramter is
+		// and anonymous function containing the actual test for each case.
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+			if hd != tt.want {
+				t.Errorf("got %q; want %q", hd, tt.want)
+			}
+		})
+	}
+
+	// // Initialize a new time.Time object and pass it to the humanDate function.
+	// tm := time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC)
+	// hd := humanDate(tm)
+
+	// // Check that the output from the humanDate function is in the format we
+	// // expect. If it isn't what we expect, use the t.Errorf() funciton to
+	// // indicate that the test has failed and log the expected and actual values.
+	// expectedResult := "17 Mar 2024 at 10:15"
+	// if hd != expectedResult {
+	// 	t.Errorf("got %q; want %q", hd, expectedResult)
+	// }
 }
